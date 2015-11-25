f704a929e5b6dc11c1272c75082926756d022108
Robinhola <elrharbi.robin@gmail.com>
2015-11-25 14:05:20 +0100

    patchList debut

--- src/patchList.c
+++ src/patchList.c
@@ -1,3 +1,95 @@
 #include "patchList.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+void printPatchList(patchList l) {
+    l = inversion(l);
+    char string[12];
+    int i = 0;
+    
+    while (l != NULL) {
+        i++;
+        printf("%d ", i);
+        
+        switch (l->op) {
+            case ADD:
+                printf("+");
+                break;
+            case DEL:
+                printf("-");
+                break;
+            case SUBST:
 
+                break;
+                
+            default:
+                break;
+        }
+        
+    }
+}
+
+patchList addHead(patchList l, operation op) {
+    // creation
+    patchListElement* new = malloc(sizeof(patchListElement));
+    new->op = op;
+    new->next = l;
+    new->nRef = 1;
+    
+    // actualisation
+    if(l!=NULL)
+        incRef(l);
+    
+    return new;
+}
+
+patchList delHead(patchList l) {
+    patchList old = l->next;
+    
+    if (old!=NULL)
+        decRef(old);
+    
+    free(l);
+    
+    return old;
+    
+}
+
+void incRef(patchList l) {
+    l->nRef++;
+}
+
+void decRef(patchList l) {
+    l->nRef--;
+    
+    if (l->nRef == 0) {
+        delHead(l);
+    }
+}
+
+patchList inversion(patchList l) {
+    
+    if (l == NULL) {
+        return l;
+    }
+    
+    patchList pointeur = l, precPointeur = NULL, nextPointeur = pointeur->next;
+    
+    while (nextPointeur != NULL) {
+        // <-P A-> N-> devient <-P <-A N->
+        pointeur->next =  precPointeur;
+        
+        // <-P <-A N-> devient <-A N-> N'->
+
+        precPointeur = pointeur;
+        
+        pointeur = nextPointeur;
+        
+        nextPointeur = nextPointeur->next;
+    }
+    
+    // <-P A-> X devient <-P <-A
+    pointeur->next = precPointeur;
+    
+    return pointeur;
+}
--- src/patchList.h
+++ src/patchList.h
@@ -12,8 +12,13 @@ typedef struct patchList_s{
 typedef patchListElement *patchList;
 
 void printPatchList(patchList l);
+
 patchList addHead(patchList l, operation op);
+patchList delHead(patchList l);
+
 void incRef(patchList l);
 void decRef(patchList l);
+
+patchList inversion(patchList l);
 
 #endif
